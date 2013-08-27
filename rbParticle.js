e2f60848387c31b0da156c166b5be6614a7da372
R1cebank <r1cebank@Hachune-2.local>
2013-08-27 19:51:48 +0800

    Added Emitter [Not Working]
    Add emitter, which broke the particle system. Will rewrote the system
    tomorrow..

--- rbParticle.js
+++ rbParticle.js
@@ -16,14 +16,19 @@ document.body.appendChild( stats.domElement );
 canvas.width = 400;
 canvas.height = 400;
 
+/*
+	Renderer
+*/
+
+var rdr = new Renderer ();
+var DELTA = 0.1;
+var totalParticles = 200;
+
 //Particles
-var particles = [];
+//var particles = [];
 
 //Particle Param
 
-var emitterConfig = {
-	totalParticles: 200
-};
 var emitterPosition = {
 	x: canvas.width / 2,
 	y: canvas.height / 2
@@ -80,44 +85,104 @@ Particle.prototype.update = function(dt) {
 
 //////////////Emitter Engine Code/////////////////
 
-function Emitter(config) {
-	apply(this, config);
+function Emitter(/*config*/) {
+	//apply(this, config);
 	
 	this.particlePool = [];
 	
-	for(var i = 0; i< emitterConfig.totalParticles; i++) {
+	this.particleCount = 0;
+	
+	for(var i = 0; i< totalParticles; i++) { ////TODO: FIX
 		this.particlePool.push(new Particle());
 	}
 	
 }
 
-function loop() {
-	clear();
-	if(stop) {
-		stop = 0;
-		return;
+Emitter.prototype.update = function (delta) {
+	while(this.shouldEmitSomeParticles()) {
+		this.addParticle(); //Particle Count Updates Here
 	}
-	addParticle();
-	draw();
-	queue();
+	
+	var particleIndex = 0;
+	while(particleIndex < this.particleCount) {
+		var particle = this.particlePool[particleIndex];
+		this.updateParticle(delta, particle, particleIndex);
+	}
+};
+
+Emitter.prototype.shouldEmitSomeParticles = function () {
+	return (this.particleCount < totalParticles);
+};
+
+Emitter.prototype.getParticleFromPool = function () {
+	return this.particlePool[this.particleCount];
+};
+
+Emitter.prototype.addParticle = function () {
+	var particle = this.getParticleFromPool();
+	this.particleCount++;
+};
+
+Emitter.prototype.returnParticleToPool = function (index) {
+	var deadParticle = this.particlePool[index];
+	this.particlePool[index] = this.particlePool[this.particleCount - 1];
+	this.particlePool[this.particleCount - 1] = deadParticle;
+};
+
+Emitter.prototype.updateParticle = function (delta, particle, particleIndex) {
+	particle.life -= delta;
+		
+	if(particle.life > 0) {
+	
+		var ageRatio = particle.life / particle.originalLife;
+		particle.size = particle.originalSize * ageRatio;
+		particle.alpha = ageRatio;
+		particle.position.x += particle.velocity.x * dt;
+		particle.position.y += particle.velocity.y * dt;
+		
+		//Move Particle Here and change particle properties here.
+	} else {
+		this.returnParticleToPool(particleIndex);
+	}
+};
+
+/////////////Ended Emitter Code/////////////
+
+////////////Start Renderer Code///////////////
+
+function Renderer () {
+	this.emitter = new Emitter();
 }
 
-function queue() {
-	window.requestAnimationFrame(loop);
-}
+/*Renderer.prototype = {
+	/*update: function () {
+		this.emitter.update(DELTA);
+	},
+	draw: function () {
+		stats.begin();
+		for(var i = 0; i < particles.length; i++) {
+			this.emitter.particlePool[i].update(0.1);
+			var part = particles[i];
+			var position = particles[i].position;
+			if(particles[i].life <= 0) continue;
+				ctx.fillStyle = 'rgba('+ part.r + ',' + part.g + ',' + part.b +','+ part.alpha +')';
+				ctx.beginPath();
+				ctx.arc(position.x, position.y, part.size, 0, Math.PI*2, true);
+				ctx.fill();		
+		}
+		stats.end();
+	}
+};*/
 
-function addParticle() {
-	particles.push(new Particle(emitterPosition.x, emitterPosition.y, particlesParam.life, Math.random() * 360 - 1, Math.random() * (20-8) + 8, particlesParam.size, startColor));
-}
+Renderer.prototype.update = function () {
+	//Do something
+	this.emitter.update(DELTA);
+};
 
-function clear() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function draw() {
+Renderer.prototype.draw = function () {
 	stats.begin();
 	for(var i = 0; i < particles.length; i++) {
-		particles[i].update(0.1);
+		this.emitter.particlePool[i].update(0.1);
 		var part = particles[i];
 		var position = particles[i].position;
 		if(particles[i].life <= 0) continue;
@@ -127,7 +192,29 @@ function draw() {
 			ctx.fill();		
 	}
 	stats.end();
+};
+
+
+////Renderer will handle emitter and call emitter functions. Renderer will draw the particles in the
+////emitter's particlePool
+
+function loop() {
+	clear();
+	if(stop) {
+		stop = 0;
+		return;
+	}
+	rdr.update();
+	//rdr.draw();
+	//addParticle();
+	//draw();
+	queue();
 }
+
+function queue() {
+	window.requestAnimationFrame(loop);
+}
+
 
 function debug (param) {
 	switch(param) {
